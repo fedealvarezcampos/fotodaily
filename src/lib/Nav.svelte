ce682451a2334043993d70547661c3d03867b33f
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-04 15:51:25 +0100

    More to learning Svelte

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,34 @@
+<script>
+	let siteTitle = 'MY SITE';
+</script>
+
+<header>
+	<p>{siteTitle}</p>
+	<nav class="title">
+		<ul>
+			<li>
+				<a href="/">HOME</a>
+			</li>
+			<li>
+				<a href="/about">ABOUT</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="postcss">
+	header {
+		display: flex;
+		place-content: center;
+
+		ul {
+			display: flex;
+			gap: 0.8rem;
+			list-style: none;
+		}
+
+		li {
+			font-weight: 800;
+		}
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,22 @@
 <script>
 	import '../app.css';
+
+	import Nav from '$lib/Nav.svelte';
 </script>
 
-<slot />
+<Nav />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>This is a cool footer | {new Date().getFullYear()}</p>
+</footer>
+
+<style lang="postcss">
+	footer {
+		display: flex;
+		place-content: center;
+	}
+</style>
