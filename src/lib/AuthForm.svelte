2aaa544148993c14040c4727d939c07fb9370088
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-14 22:23:55 +0100

    AuthForm and Modal

---
+++ src/lib/AuthForm.svelte
@@ -0,0 +1,91 @@
+<script>
+	import axios from 'axios';
+	import { debug } from 'svelte/internal';
+
+	const hostURL = import.meta.env.VITE_HOSTURL;
+
+	let email;
+	let password;
+	let confirmPassword;
+
+	let err;
+
+	const handleSubmit = () => {
+		console.log('something');
+	};
+
+	const handleRegistration = async () => {
+		try {
+			if (password !== confirmPassword) {
+				err = "Passwords don't match!";
+				return;
+			}
+
+			const { data, error } = await axios.post(`${hostURL}/api/auth/local/register`, {
+				email: email,
+				password: password
+			});
+
+			if (error) throw error;
+
+			localStorage.user = JSON.stringify({
+				jwt: data?.jwt,
+				userId: data?.user?.id,
+				email: data?.user?.email,
+				confirmed: data?.user?.confirmed
+			});
+		} catch (error) {
+			err = error.message;
+		}
+	};
+</script>
+
+<form action="/" on:submit|preventDefault={handleRegistration}>
+	<span>REGISTER</span>
+	<div class="inputs">
+		<label for="mail">
+			Your email
+			<input bind:value={email} type="email" name="mail" id="mail" />
+		</label>
+		<label for="pass">
+			Your password
+			<input bind:value={password} type="password" name="pass" id="pass" />
+		</label>
+		<label for="confirmPass">
+			Confirm password
+			<input bind:value={confirmPassword} type="password" name="confirmPass" id="confirmPass" />
+		</label>
+	</div>
+	{err}
+	<button>SIGN IN</button>
+</form>
+
+<style lang="postcss">
+	form {
+		display: flex;
+		flex-direction: column;
+		place-items: flex-start;
+		gap: 1.5rem;
+
+		span:first-child {
+			font-size: 1.3rem;
+			font-weight: 700;
+		}
+
+		button {
+			margin-top: 0.5rem;
+		}
+
+		.inputs {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+
+			> label {
+				display: flex;
+				flex-direction: column;
+				gap: 0.6rem;
+			}
+		}
+	}
+</style>
---
+++ src/lib/Modal.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let showModal = false;
+</script>
+
+{#if showModal}
+	<div class="modal">
+		<slot />
+	</div>
+	<div class="modalBG" on:click|self />
+{/if}
+
+<style lang="postcss">
+	.modalBG {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		backdrop-filter: blur(4px);
+		background-color: rgba(0, 0, 0, 0.39);
+		z-index: 3;
+	}
+
+	.modal {
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		height: fit-content;
+		width: fit-content;
+		margin: auto;
+		padding: 2.55rem 2.25rem 2.6rem 2.2rem;
+		border: 6px solid var(--pink);
+		border-radius: 8px;
+		background-color: var(--red);
+		z-index: 4;
+	}
+</style>
